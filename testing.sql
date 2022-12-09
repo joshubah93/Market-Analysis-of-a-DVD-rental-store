a572ab6f331932b72316c6154fa20bceb54b70fd
Joshua <54960680+joshubah93@users.noreply.github.com>
2022-12-09 10:06:59 +0100

    Delete testing.sql

--- testing.sql
+++
@@ -1,6 +0,0 @@
-SELECT cd.bookings.starttime 
-FROM cd.bookings 
-INNER JOIN cd.members ON 
-cd.members.memid = cd.bookings.memid 
-WHERE cd.members.firstname='David' 
-AND cd.members.surname='Farrell';
